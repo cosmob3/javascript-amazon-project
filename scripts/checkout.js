0f80fe8eb2d594e35e21ac38f90a39be60fff590
cosmob3 <3ryanvelasc0@gmail.com>
2025-06-09 00:34:51 -0600

    remove html when pressing delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -12,7 +12,9 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
     }
   });
-  cartSummaryHTML += `<div class="cart-item-container">
+  cartSummaryHTML += `<div class="cart-item-container js-cart-item-container ${
+    matchingProduct.id
+  }">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -91,5 +93,10 @@ document.querySelectorAll(".js-delete-quantity").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
