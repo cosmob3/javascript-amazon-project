3c8d05f7e1eafc81d4feb1bef5d71b08d105ccb7
cosmob3 <3ryanvelasc0@gmail.com>
2025-06-09 00:20:48 -0600

    fixed radio buttons and money rounding error

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,7 @@
 // Module
 import { addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsHTML = "";
 let addedTimeOut;
@@ -30,9 +31,9 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">$${(product.priceCents / 100).toFixed(
-            2
-          )}</div>
+          <div class="product-price">$${formatCurrency(
+            product.priceCents
+          )} </div>
 
           <div class="product-quantity-container">
             <select class="js-quantity-selector-${product.id}" >
